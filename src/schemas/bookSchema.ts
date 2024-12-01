40d9e328fec0b43ad1920f8a63b9a73688d6a676
Yunus Taha Yılmaz <48367134+yunustaha@users.noreply.github.com>
2024-12-01 21:02:24 +0300

    Added validation layer to bookSchema

--- src/schemas/bookSchema.ts
+++ src/schemas/bookSchema.ts
@@ -2,13 +2,13 @@ import Joi from "joi";
 
 const postBook = Joi.object({
   body: Joi.object({
-    name: Joi.string().min(3).max(100).required(),
+    name: Joi.string().min(3).max(100).required().label("name"),
   }).required(),
 });
 
 const getBook = Joi.object({
   params: Joi.object({
-    id: Joi.number().integer().positive().required(),
+    id: Joi.number().integer().positive().required().label("id"),
   }).required(),
 });
 
